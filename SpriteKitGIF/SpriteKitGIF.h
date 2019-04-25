cf2ac27722138965a19bbb52cea0849c13d54cf2
Jacob Boyd <jacobboyd14012@gmail.com>
2019-04-25 14:03:58 -0400

    Added Cocopods

---
+++ SpriteKitGIF/SpriteKitGIF.h
@@ -0,0 +1,19 @@
+//
+//  SpriteKitGIF.h
+//  SpriteKitGIF
+//
+//  Created by Jacob Boyd on 4/25/19.
+//  Copyright © 2019 Jacob Boyd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SpriteKitGIF.
+FOUNDATION_EXPORT double SpriteKitGIFVersionNumber;
+
+//! Project version string for SpriteKitGIF.
+FOUNDATION_EXPORT const unsigned char SpriteKitGIFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpriteKitGIF/PublicHeader.h>
+
+
