afe9d4041eb40bafef9513b64798cbf8012e4806
Jacob Boyd <jacobboyd14012@gmail.com>
2019-04-25 15:26:44 -0400

    added base GIFView

---
+++ SpriteKitGIF/SpriteKitGIFView.swift
@@ -0,0 +1,13 @@
+//
+//  SpriteKitGIFView.swift
+//  SpriteKitGIF
+//
+//  Created by Jacob Boyd on 4/25/19.
+//  Copyright © 2019 Jacob Boyd. All rights reserved.
+//
+
+import UIKit
+
+class SpriteKitGIFView: SKScene {
+
+}
